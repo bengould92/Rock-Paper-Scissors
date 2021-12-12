e53ec6929192b1c2a69132077de04c315a53ee12
Ben <93551687+bengould92@users.noreply.github.com>
2021-12-12 02:12:45 +0000

    Add project files.

---
+++ Rock, Paper, Scissors/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Rock__Paper__Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS") // While player var does not equal rock, paper or scissors continue asking for input
+                {
+                    Console.Write("Enter ROCK, PAPER or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                }
+
+                
+                switch (random.Next(1, 4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break ;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
